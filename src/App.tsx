7cc0918424a9111071835335354a9ed2cabdb69d
Dicky Kitchen Jr <80136142+DickyKitchenJr@users.noreply.github.com>
2024-06-17 19:36:57 -0400

    Update App.tsx
    set up header and displayed MathProblem 15 times to provide the user with 15 different equations to solve

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
-import './App.css'
-import MathProblem from './components/MathProblem'
+import "./App.css";
+import MathProblem from "./components/MathProblem";
+import Header from "./components/Header";
 
 function App() {
-  
+  //used to set how many problems will be displayed
+  const numberOfProblems = 15;
 
   return (
     <>
-      <MathProblem />
+      <Header />
+      <div className="problems">
+        {Array.from({ length: numberOfProblems }, (_, index) => (
+          <div key={index} className="problem-box">
+            {index + 1}.<MathProblem />
+          </div>
+        ))}
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
